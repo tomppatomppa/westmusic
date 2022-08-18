652b65f732d90b8c3c8a4857fca208d78a0aa8dd
tomi <tomiwest@hotmail.com>
2022-08-18 18:13:05 +0300

    import google fonts

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefine Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+    },
+  },
+  plugins: [],
+}
