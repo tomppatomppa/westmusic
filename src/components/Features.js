6ca5864c869e19255ef720929e1c643db3110318
tomi <tomiwest@hotmail.com>
2022-08-19 11:46:36 +0300

    features opacity

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,8 +4,8 @@ const Features = () => {
   return (
     <div className="relative container flex flex-col max-w-6xl mx-auto my-64 px-6 text-gray-900 md:flex-row md:px-0">
       <img src={tika} alt=""></img>
-      <div className=" top-72 pr-0 bg-indigo-100 rounded  md:absolute  md:right-0 md:py-20 md:pl-20">
-        <h2 className="max-w-lg mt-10 mb-6 font-sans text-4xl text-center text-gray-900 uppercase md:text-5xl md:mt-0 md:text-left">
+      <div className="opacity-80 top-72 pr-0 bg-black text-white rounded  md:absolute  md:right-0 md:py-20 md:pl-20">
+        <h2 className="max-w-lg mt-10 mb-6 font-sans text-4xl text-center   uppercase md:text-5xl md:mt-0 md:text-left">
           The Live Show You Always Wanted
         </h2>
         <p className="max-w-md text-center md:text-left">
