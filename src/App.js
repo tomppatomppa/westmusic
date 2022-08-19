877c11eb9a3bde9be0ef9cdca37c5a1ed55cbcf6
tomi <tomiwest@hotmail.com>
2022-08-19 10:01:43 +0300

    hero video

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import './index.css'
 import { Link } from 'react-router-dom'
-import herovideo from './videos/hero_video.mp4'
+
 import Features from './components/Features'
 import Content from './components/Content'
-
+import ReactPlayer from 'react-player'
+import Hero from './components/Hero'
 function App() {
   return (
     <div>
       <div id="hero">
-        <div className="container max-w-6xl mx=auto px-0 py-12 text-white">
-          {
-            <nav className="flex items-center justify-between font-bold">
-              <h1 className="px-10">West Music</h1>
-              <div className="hidden h-10 font-alata md:flex md:space-x-8">
-                <div className="group">
-                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
-                  <Link to="/contact">About</Link>
-                </div>
-                <div className="group">
-                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
-                  <Link to="/contact">Contact</Link>
-                </div>
-              </div>
-            </nav>
-          }
-          <div className="video-container">
-            <video id="video-bg" autoPlay loop muted>
-              <source src={herovideo} type="video/mp4"></source>
-            </video>
-          </div>
-
-          <div className="max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2 md:p-10 md:m-32 md:mx-0 md:text-6xl">
-            Events that you deserve
-          </div>
-        </div>
+        <Hero />
       </div>
       <Features id="feature" />
       <Content id="content" />
---
+++ src/components/Hero.js
@@ -0,0 +1,40 @@
+import ReactPlayer from 'react-player'
+import { Link } from 'react-router-dom'
+import herovideo from '../videos/hero_video.mp4'
+const Hero = () => {
+  return (
+    <div classname="relative">
+      <div className="overlay"></div>
+      <ReactPlayer
+        className="max-h-96"
+        id="video"
+        url={herovideo}
+        playing
+        loop
+        muted
+        width="100%"
+        height="100%"
+      />
+
+      <nav className="content absolute flex items-center  top-0 h-1/5 w-11/12 justify-between font-bold text-white ">
+        <h1 className="px-10 uppercase ">West Music</h1>
+        <div className="hidden h-10 font-alata md:flex md:space-x-8 pr-10">
+          <div className="group">
+            <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+            <Link to="/contact">About</Link>
+          </div>
+          <div className="group">
+            <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="content relative bottom-0 top-0 max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2 md:p-10 md:m-32 md:mx-0 md:text-6xl">
+        Events that you deserve
+      </div>
+    </div>
+  )
+}
+
+export default Hero
